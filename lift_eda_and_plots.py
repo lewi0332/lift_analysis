57b8b21b64f263c9cff5701a63feb759b88f65c0
derricklewis <derrickjameslewis@gmail.com>
2019-09-15 14:16:03 -0400

    add converted function

--- lift_eda_and_plots.py
+++ lift_eda_and_plots.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import plotly
+import datetime
 
 '''Load data from CSV'''
 df = pd.read_csv(
@@ -32,16 +33,6 @@ df.loc[df.event_type == 'halted'].count()
 df.loc[df.event_type == 'delivered'].count()
 df.loc[df.event_type == 'purchase'].count()
 
-"""Looking at one users journey"""
-one_user = df.loc[df.email == 6024938649550848].copy()
-
-
-purchases = df.loc[df.event_type == 'purchase'].copy()
-converted = df[df.email.isin(list(purchases.email))].copy()
-
-
-'''Maybe one hot encode the event type?'''
-
 """ 
 Need to look for 5 day window to determine purchase conversion rate
 Using pandas.to_datetime to clean event_date column 
@@ -49,3 +40,73 @@ Using pandas.to_datetime to clean event_date column
 
 df.event_date = pd.to_datetime(df.event_date, format="%Y-%m-%d")
 df.event_date = df.event_date.dt.round("D")
+
+
+"""Looking at one users journey"""
+one_user = df.loc[df.email == 5312915884212224].copy()
+
+
+'''Maybe one hot encode the event type?'''
+
+""" 
+Start with a list of delivered and a list of halted.
+Take a count of each list
+Gather email from those. Match emails to purchased and add.
+Filter purchases frome within 5 days of email
+count purchases that meet the criteria
+Divide by total purchases and control.  
+"""
+
+big_bc_list = df.loc[df.event_type == 'delivered'].copy()
+big_bc_list.email.nunique()  # 506406
+
+big_control_list = df.loc[df.event_type == 'halted'].copy()
+big_control_list.email.nunique()  # 28596
+
+bc = df[(df.email.isin(list(big_bc_list.email))) &
+        (df.event_type == 'purchase')].copy()
+bc['converted'] = 0
+control = df[(df.email.isin(list(big_control_list.email)))
+             & (df.event_type == 'purchase')].copy()
+control['converted'] = 0
+
+
+# not pretty. not fast.
+
+def converted(emails, purchases, type):
+    '''
+    Dates are a challenge. Function takes two lists and type of email 
+    Start with each purchased date and validate an email delivery date 
+    is less than or equal than 5 days.
+    append the purchased list with a 1 to mark that this purchase was converted 
+    from an email (or control) 
+    '''
+
+    for i in range(len(purchases)):
+        print(i)
+        test = False  # reset test to false
+
+        delivered_dates = emails.event_date.loc[(
+            emails.event_type == type) & (emails.email == purchases.email.iloc[i])]
+        # Gather all the email delivered dates from the person who purchased
+
+        for _ in range(len(delivered_dates)):
+            if test is False:
+                test = purchases.iloc[i]['event_date'] - \
+                    delivered_dates.iloc[_] <= datetime.timedelta(days=5) and \
+                    purchases.iloc[i]['event_date'] - \
+                    delivered_dates.iloc[_] > datetime.timedelta(days=0)
+                # Test to see if there is an email within 5 days before purchasing
+            else:
+                purchases['converted'].iloc[i] = 1
+
+
+converted(big_bc_list, bc, 'delivered')
+
+converted(big_control_list, control, 'halted')
+
+
+'''Do two purchases by the same person count as being converted twice?'''
+
+bc['converted'].sum()
+control['converted'].sum()
