836f89d4f5ac23697185366b850fd900e06b784d
derricklewis <derrickjameslewis@gmail.com>
2019-09-15 22:04:13 -0400

    email conversion change

--- lift_eda_and_plots_1_day.py
+++ lift_eda_and_plots_1_day.py
@@ -1,22 +1,21 @@
 import pandas as pd
+import plotly
 import datetime
 
 '''Load data from CSV'''
 df = pd.read_csv(
     '/Users/derricklewis/Documents/Flatiron/bluecore/lift_analysis/lift_analysis_assignment.csv')
 
-
 ''' initial look at the data'''
-df.head()
 df.describe()
 
 df.info()
 '''
 df.info() shows that the total column is not an integer. With
-investigation there are totals that are "MISSING". For our first
-questions of conversion, I will choose to include these rows in the
+investigation there are totals that are "Missing". For our first
+questions of conversion, I will choose to include these users in the
 lift conversions, but will need to clean this later when looking at
-total revenue
+total sales
 '''
 
 df.head()
@@ -60,11 +59,9 @@ Divide by total purchases and control.
 
 big_bc_list = df.loc[df.event_type == 'delivered'].copy()
 big_bc_list.email.nunique()  # 506406
-big_bc_list['converted'] = 0
 
 big_control_list = df.loc[df.event_type == 'halted'].copy()
 big_control_list.email.nunique()  # 28596
-big_control_list['converted'] = 0
 
 bc = df[(df.email.isin(list(big_bc_list.email))) &
         (df.event_type == 'purchase')].copy()
@@ -76,7 +73,7 @@ control['converted'] = 0
 
 # not pretty. not fast.
 
-def add_converted(emails, purchases, type):
+def converted(emails, purchases, type):
     '''
     Dates were a challenge. This function inputs two lists and a cohort type
     Start with each purchased date and validate an email delivery date
@@ -89,14 +86,14 @@ def add_converted(emails, purchases, type):
         print(i)
         test = False  # reset test to false
 
-        delivered_dates = emails.event_date.loc[emails.email ==
-                                                purchases.email.iloc[i]]
+        delivered_dates = emails.event_date.loc[(
+            emails.event_type == type) & (emails.email == purchases.email.iloc[i])]
         # Gather all the email delivered dates from the person who purchased
 
         for _ in range(len(delivered_dates)):
             if test is False:
                 test = purchases.iloc[i]['event_date'] - \
-                    delivered_dates.iloc[_] <= datetime.timedelta(days=5) and \
+                    delivered_dates.iloc[_] <= datetime.timedelta(days=1) and \
                     purchases.iloc[i]['event_date'] - \
                     delivered_dates.iloc[_] > datetime.timedelta(days=0)
                 # Test to see if there is an email within 5 days before purchasing
@@ -105,20 +102,15 @@ def add_converted(emails, purchases, type):
 
 
 '''Run the function on both the bc and control lists'''
-add_converted(big_bc_list, bc)
-add_converted(big_control_list, control)
-
-
-'''Run the function in reverse to see email effectiveness'''
-add_converted(bc, big_bc_list)
-add_converted(control, big_control_list)
+converted(big_bc_list, bc, 'delivered')
+converted(big_control_list, control, 'halted')
 
 
 '''Do two purchases by the same person count as being converted twice?'''
 
 
 '''Print results'''
-# -----------------------------------------------------------------------
+
 ''' Number of converted purchases'''
 bc_purchases_conversion = (bc['converted'].sum() / len(bc))
 control_purchase_conversion = (
@@ -126,13 +118,11 @@ control_purchase_conversion = (
 purchase_lift = ((bc_purchases_conversion - control_purchase_conversion) /
                  control_purchase_conversion)
 print("Purchase Conversion rate")
-print(
-    f".....BC Cohort: {bc['converted'].sum()} of {len(bc)} for {bc_purchases_conversion:.2%}")
-print(
-    f".....Control Cohort: {control['converted'].sum()} of {len(control)} for {control_purchase_conversion:.2%}")
+print(f".....BC Cohort: {bc_purchases_conversion:.2%}")
+print(f".....Control Cohort: {control_purchase_conversion:.2%}")
 print(f"This created a {purchase_lift:.2%} lift in conversion")
 
-# -----------------------------------------------------------------------
+
 '''Number of converted customers'''
 only_converted_purchases_bc = bc.loc[bc.converted == 1].copy()
 only_converted_purchases_control = control.loc[control.converted == 1].copy()
@@ -147,21 +137,19 @@ print(f".....BC Cohort: {bc_cust_conversion:.2%}")
 print(f".....Control Cohort: {control_cust_conversion:.2%}")
 print(f"This created a {customer_lift:.2%} lift in conversion")
 
-# -----------------------------------------------------------------------
+
 ''' Number of converted emails'''
-bc_email_conversion = (big_bc_list['converted'].sum() / len(big_bc_list))
+bc_email_conversion = (bc['converted'].sum() / big_bc_list.email.nunique())
 control_email_conversion = (
-    big_control_list['converted'].sum() / len(big_control_list))
+    control['converted'].sum() / big_control_list.email.nunique())
 email_lift = ((bc_email_conversion - control_email_conversion) /
               control_email_conversion)
 print("\n\nEmail Conversion rate")
-print(
-    f".....BC Cohort: {big_bc_list['converted'].sum()} of {len(big_bc_list)} for {bc_email_conversion:.2%}")
-print(
-    f".....Control Cohort: {big_control_list['converted'].sum()} of {len(big_control_list)} for {control_email_conversion:.2%}")
+print(f".....BC Cohort: {bc_email_conversion:.2%}")
+print(f".....Control Cohort: {control_email_conversion:.2%}")
 print(f"This created a {email_lift:.2%} lift in conversion")
 
-# -----------------------------------------------------------------------
+
 '''Revenue attributed to emails
 -First remove all values named "missing" and less than 0 on both lists
 '''
